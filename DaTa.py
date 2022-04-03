df456e06d4e7d6b1bf1ae3b8d9ec327d678a3c9c
Vladimir <59509835+sleepy-moth@users.noreply.github.com>
2022-04-03 13:11:00 +0300

    Add files via upload

---
+++ DaTa.py
@@ -0,0 +1,5 @@
+class DaTa():
+    def __init__(self, nickname, login, password ):
+        self.name = nickname
+        self.login = login
+        self.password = password
---
+++ test_chat.py
@@ -0,0 +1,38 @@
+
+import telebot
+import DaTa # Класс с данными юзера
+import Confid # Хранение Токена(config)
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+bot = telebot.TeleBot(Confid.TOKEN, parse_mode=None)
+bot_aio = Bot(token=Confid.TOKEN)
+dp = Dispatcher(bot_aio)
+
+
+@bot.message_handler(commands=['start']) #Начальное приветствие
+def start_message(message):
+    bot.send_message(message.from_user.id,"Здаров, зарегистрируйся по братски")
+    bot.send_message(message.from_user.id, "Введите логин")  # Запрос Логина
+
+    @bot.message_handler()
+    def name_login(message: types.Message):
+        DaTa.login = message.text
+        bot.send_message(message.from_user.id,
+                            "Твой логин {}? Ладно".format(DaTa.login)) # Подтверждение логина и ввод в перменную класса DaTa
+        bot.send_message(message.from_user.id, "Введите пароль") # Запрос пароля
+    @bot.message_handler()
+    def name_password(message: types.Message):
+        DaTa.password = message.text
+        bot.send_message(message.from_user.id,
+                             "Значит твой пароль {}?".format(
+        DaTa.password)) #Ввод пароля и перенос в переменную класса DaTa
+
+
+@bot.message_handler(commands=['help']) #
+def help_message(message):
+    bot.send_message(message.from_user.id, "I don't know what to wright here right now")
+
+
+bot.polling(none_stop=True, interval=0)
